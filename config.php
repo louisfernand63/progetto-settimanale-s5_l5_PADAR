dca64e6a92c742f2c5423dfaa5d92d83e532949a
louisfernand63 <108729182+louisfernand63@users.noreply.github.com>
2024-03-06 11:56:10 +0100

    up to date 1

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+progetto settimanale s5_l5_PADAR:
+NomeDb : padar
+Nome utente : root
+Nome host: localhost
+password: vu-86s7qLe-bDRW@
+ */
+
+ 
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'padar',
+    'port' => '3306',
+    'user' => 'root',
+    'password' => 'vu-86s7qLe-bDRW@'
+];
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once('database.php');
+$config = require_once('config.php');
